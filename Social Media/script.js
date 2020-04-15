8951eb797fe209eaf5743a5142a1dee73ddc5990
Emerson-e-coder <63274859+Emerson-e-coder@users.noreply.github.com>
2020-04-15 05:39:40 -0300

    Social Media

---
+++ Social Media/script.js
@@ -0,0 +1,64 @@
+
+function imgpret() {
+    document.getElementById('imgyt').src="imagens/youtube02.png";
+}
+function imgbranc() {
+    document.getElementById('imgyt').src="imagens/youtube.png";
+}
+
+
+
+function imgpreta() {
+    document.getElementById('icone').src="imagens/whats02.png";
+}
+function imgbranca() {
+    document.getElementById('icone').src="imagens/whats.png";
+}
+
+
+function imgpre() {
+    document.getElementById('icon').src="imagens/insta02.png";
+}
+function imgbran() {
+    document.getElementById('icon').src="imagens/insta.png";
+}
+
+
+function imgpr() {
+    document.getElementById('ico').src="imagens/linkedin02.png";
+}
+function imgbra() {
+    document.getElementById('ico').src="imagens/linkedin.png";
+}
+
+
+function imgp() {
+    document.getElementById('git').src="imagens/github02.png";
+}
+function imgb() {
+    document.getElementById('git').src="imagens/github.png";
+}
+
+
+function imgpretaa() {
+    document.getElementById('tum').src="imagens/tumblr02.png";
+}
+function imgbrancaa() {
+    document.getElementById('tum').src="imagens/tumblr.png";
+}
+
+
+function ipreta() {
+    document.getElementById('face').src="imagens/facebook02.png";
+}
+function ibranca() {
+    document.getElementById('face').src="imagens/facebook.png";
+}
+
+
+function immgpreta() {
+    document.getElementById('tiwt').src="imagens/twitter02.png";
+}
+function immgbranca() {
+    document.getElementById('tiwt').src="imagens/twitter.png";
+}
